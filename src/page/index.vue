4f75a0ee425bdf1af663d715958b62d45557c10d
earl <z604458675@gmail.com>
2018-06-07 18:18:33 +0800

    添加视频播放

--- src/page/index.vue
+++ src/page/index.vue
@@ -4,24 +4,42 @@
     Description: 首页
 -->
 <template>
-    <div class="index">
-      这是首页
-    </div>
+  <div class="index">
+    <my-video :sources="video.sources" :options="video.options" :errorMsg="video.errorMsg"></my-video>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {}
+  import myVideo from 'vue-media-player'
+
+  export default {
+    data() {
+      return {
+        video: {
+          sources: [{
+            src: 'http://zxxb-oss.haibian.com/2018/4/28/MP4/1524924727703.mp4',
+            type: 'video/mp4'
+          }],
+          options: {
+            autoplay: false,
+            volume: 0.6,
+            poster: 'https://github.com/EARL8888/SrcPath/blob/master/video/video_poster.jpg?raw=true'
+          },
+          errorMsg: '视频加载失败!'
         }
+      }
+    },
+    components: {
+      myVideo
     }
+  }
 </script>
 
 <style scoped>
 
-  .index{
-    width: 200px;
-    height: 200px;
+  .index {
+    width: 500px;
+    height: 500px;
     background: green;
     position: absolute;
     left: 0;
